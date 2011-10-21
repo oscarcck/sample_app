f5a9640fc8047ca6ef4da6aabba693aac2a719f8
RacsO <oscarcck@gmail.com>
2011-10-21 19:03:57 +0800

    again

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,24 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
+  
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+  
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
 
-  describe "GET 'new'" do
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  end
+
+  describe "GET 'new'" do    
     it "should be successful" do
       get 'new'
       response.should be_success
@@ -14,5 +30,4 @@ describe UsersController do
       response.should have_selector("title", :content => "Sign up")
     end
   end
-
 end
